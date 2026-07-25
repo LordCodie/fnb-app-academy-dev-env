cf4ed5098e90b84bd5185328b0b5d1d3ea0029b4
Bafana Baloyi <153275673+LordCodie@users.noreply.github.com>
2026-07-25 19:50:01 +0000

    saving progress

---
+++ app_academy/unit_1/challenge.py
@@ -0,0 +1,7 @@
+print("Please enter your name:") 
+user_name = input("")
+
+print("Please enter the name of the artist:")
+artist_name = input("")
+
+print(f"Hey {user_name}! Your tickets to see {artist_name} are booked successfully!")
---
+++ app_academy/unit_2/practical_task.py
@@ -0,0 +1,11 @@
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+bio_message = input("Please enter your bio message: ")
+
+username = f"{first_name[0].lower() + last_name.lower()}"
+
+print(f"Full Name: {(first_name + " " + last_name).title()}")
+
+print(f"Number of characters in bio: {len(bio_message)}")
+
+print(f"username {username}:\n {bio_message.strip().replace("I am", "I'm")}")
